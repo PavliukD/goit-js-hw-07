23ca1bf74d427537c43a631952d80c71e92451c2
DmitriyPavlyuk <albatrozzz1@gmail.com>
2021-08-10 20:09:49 +0300

    исправлена ошибка в задаче 6

--- js/task-06.js
+++ js/task-06.js
@@ -6,8 +6,10 @@ const inputElLength = Number(inputEl.dataset.length)
 inputEl.addEventListener('blur', validationCheck)
 
 function validationCheck(event) {
+    console.log('blur')
     if (inputElLength === event.currentTarget.value.length) {
-       return inputEl.classList.add("valid")
+        inputEl.classList.remove("invalid")
+        return inputEl.classList.add("valid")
     }
 
     inputEl.classList.add("invalid")
